f04479bd3e0af13f60aad2478b79899d17cd1fa4
luuvan1207 <1983luunganvan@gmail.com>
2026-08-17 19:35:23 +0700

    update build script

--- build_dynamic.sh
+++ build_dynamic.sh
@@ -1,1 +1,2 @@
+echo "building dynamic library"
 g++-16 -Ofast -shared -fPIC src/real.cpp -o ./build/dynamiclib/libreal.so
--- build_static.sh
+++ build_static.sh
@@ -1,2 +1,3 @@
+echo "building static library"
 g++-16 -Ofast -c src/real.cpp -o build/objects/real.o
 ar rcs build/staticlib/libreal.a build/objects/real.o
